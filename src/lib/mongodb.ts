11bc2ef8b37c85898f1029a7caaadc817920f4dd
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2023-11-13 00:00:11 -0300

    fix mongodb login

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,15 +1,17 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const credentials = process.env.MONGO_CERT;
-
 let client: MongoClient;
 
 export async function connect(): Promise<MongoClient> {
   if (client) {
     return client;
   }
-  return (client = await new MongoClient(process.env.MONGO_URI || "", {
-    tlsCertificateKeyFile: credentials,
-    serverApi: ServerApiVersion.v1,
+
+  return (client = await new MongoClient(process.env.MONGODB_URI || "", {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
   }).connect());
 }
