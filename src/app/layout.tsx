d4c7b1c72146ae8df51f2867b436baa6a23536a8
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2023-11-20 15:04:45 -0300

    fixup! feat: add maptile cache as service worker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import packageJson from "../../package.json";
 import "./globals.css";
-import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
