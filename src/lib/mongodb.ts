5651f2aacb7c3e5a664732991ed0c55f92393fe7
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2023-12-01 19:00:42 -0300

    refactor mongodb client initialization

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,26 +1,8 @@
-import { Collection, Document, MongoClient, ServerApiVersion } from "mongodb";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
-let client: MongoClient;
-
-export async function connect(): Promise<MongoClient> {
-  if (client) {
-    return client;
-  }
-
-  console.log(`Connecting to MongoDB "${process.env.MONGODB_URI}"`);
-
-  return (client = await new MongoClient(process.env.MONGODB_URI || "", {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      deprecationErrors: true,
-    },
-  }).connect());
-}
-
-export async function getCollection<T extends Document>(
-  dbName: string,
-  collectionName: string
-): Promise<Collection<T>> {
-  const client = await connect();
-  return client.db(dbName).collection<T>(collectionName);
-}
+export const client = new MongoClient(process.env.MONGODB_URI || "", {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    deprecationErrors: true,
+  },
+});
