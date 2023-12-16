2c7306b2cf04282220723fc555175ed63da11129
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2023-12-15 22:22:00 -0300

    fix style of InfoPanel

--- components/InfoPanel.tsx
+++ components/InfoPanel.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import { useAtom } from "jotai";
+import { ChevronUp } from "lucide-react";
 import { useState } from "react";
 import { Copy } from "./Copy";
 import { mapInfoAtom } from "./LocationMap/atoms";
@@ -14,53 +15,43 @@ export const InfoPanel = () => {
           "sm:w-1/2 xl:w-1/3 pointer-events-auto",
           "grid grid-cols-[min-content_auto]",
           "bg-white dark:bg-slate-800 text-slate-600 dark:text-slate-300 font-light text-sm",
-          "rounded-sm shadow-md"
+          "rounded-lg shadow-md"
         )}
       >
-        <div className="flex items-end border-r-[1px] border-slate-300 dark:border-slate-950 p-2">
+        <div className="flex items-end border-r-[1px] border-slate-300 dark:border-slate-950 px-2 pb-[10px]">
           <button onClick={() => setOpen((open) => !open)}>
-            <svg
-              viewBox="0 0 100 100"
-              className={clsx("w-[18px] transition-all", {
+            <ChevronUp
+              className={clsx("transition-all", {
                 "rotate-180": open,
               })}
-            >
-              <path
-                d="M12,35 L50,75 L88,35"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={16}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+            />
           </button>
         </div>
         <div
-          className={clsx("transition-all", {
-            "h-9 overflow-hidden text-slate-400 dark:text-slate-600": !open,
+          className={clsx("m-3 transition-all", {
+            "h-5 overflow-hidden text-slate-400 dark:text-slate-600": !open,
             "h-fit max-h-96 overflow-y-auto": open,
           })}
         >
           {info && (
-            <dl className="grid grid-cols-1 lg:grid-cols-[max-content_auto] gap-2 items-start p-2">
+            <dl className="grid grid-cols-1 lg:grid-cols-[max-content_auto] gap-2 items-start">
               <dt>Bounds</dt>
-              <dd>
+              <dd className="flex flex-row items-baseline justify-between">
                 {info?.bounds.toArray().flat().join(", ")}
                 <Copy />
               </dd>
               <dt>Center</dt>
-              <dd>
+              <dd className="flex flex-row items-baseline justify-between">
                 {info.center.toArray().join(", ")}
                 <Copy />
               </dd>
               <dt>Aprox. radius (m)</dt>
-              <dd>
+              <dd className="flex flex-row items-baseline justify-between">
                 {info.radius}
                 <Copy />
               </dd>
               <dt>Zoom</dt>
-              <dd>
+              <dd className="flex flex-row items-baseline justify-between">
                 {info.zoom}
                 <Copy />
               </dd>
